f1ccbcb6830cb410a02e523074c12a5bdffc87ef
Lovino Maxime <maximelovino@gmail.com>
2017-11-30 16:59:19 +0100

    added DIMANCHE feature

--- start.js
+++ start.js
@@ -20,23 +20,28 @@ bot.on('ready', () => {
 
 bot.on('message', (message) => {
 	log.info(`${message.author.username} - ${message.content}`);
+	let result;
+	const mancheRegEx = new RegExp('(?:.*)(?:di(?:s|t)?)(.*)', 'gi');
 	if (message.content === "$clear") {
 		message.channel.fetchMessages({ limit: 100 }).then(messages => {
 			deleteAllNotPinned(messages);
 		}).catch(e => log.error(e));
+	} else if ((result = mancheRegEx.exec(message.content)) != null) {
+		log.info(`Matched message, gonna write ${result[1]}`);
+		message.channel.send(result[1].trim());
 	}
-})
+});
 
 bot.login(token);
 
 function deleteAllNotPinned(messagesCollection) {
 	const messages = Array.from(messagesCollection.values());
 	const notPinned = messages.filter(m => !m.pinned);
-	log.info(`Deleting ${notPinned.length} out of ${messages.length}`);
-
+	log.info(`Deleting ${notPinned.length} out of ${messages.length} `);
+	//we can map to the promise and wait with Promise.all afterwards
 	notPinned.forEach(message => {
 		message.delete()
-			.then(msg => log.info(`Deleted message: ${msg.content}`))
-			.catch(console.error);
+			.then(msg => log.info(`Deleted message: ${msg.content} `))
+			.catch(e => log.error(e));
 	});
 }
